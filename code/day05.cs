b60fb2fa86a84e01e8d2d43ef895e84265348332
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-12-05 19:28:36 +1300

    [2025 Day 05]

---
+++ code/day05.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Immutable;
+using MoreLinq;
+using Range = (ulong a, ulong b);
+public class Day05
+{
+    const string day = "05";
+    public static (Range[] ranges, ulong[] ids) Input() =>
+        File.ReadLines($"input/2025_{day}_input.txt").Split("").Fold((range, available) => (
+            range.Select(pair => pair.Split('-').Fold((start, end) => (ulong.Parse(start), ulong.Parse(end)))).ToArray(),
+            available.Select(id => Convert.ToUInt64(id)).ToArray())); 
+
+    public static object Solve1()
+    {
+        var input = Input();
+        int count = 0;
+        foreach (var id in input.ids)
+        {
+            foreach (var range in input.ranges)
+            {
+                if (id >= range.a && id <= range.b)
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static object Solve2()
+    {
+        List<Range> freshRanges = new();
+        List<int> rangeParts = new();
+        var input = Input();
+        foreach (var r in input.ranges)
+        {
+            rangeParts.Clear();
+            for( int i = 0; i < freshRanges.Count; i++)
+            {
+                if (Overlaps(r, freshRanges[i]))
+                {
+                    rangeParts.Add(i);
+                }
+            }
+
+            Range unionRange = r;
+            foreach( int rangePart in rangeParts)
+            {
+                unionRange = RangeUnion(unionRange, freshRanges[rangePart]);
+            }
+
+            // Remove the parts that have been combined by union
+            rangeParts.Reverse();
+            foreach ( var i in rangeParts)
+            {
+                freshRanges.RemoveAt(i);
+            }
+
+            freshRanges.Add(unionRange);
+        }
+
+        ulong count = 0;
+        foreach(var r in freshRanges)
+        {
+            count += RangeLength(r);
+        }
+
+        return count;
+    }
+
+    public static bool Overlaps(Range x, Range y) 
+        => y.b >= x.a && y.a <= x.b 
+        || x.b >= y.a && x.a <= y.b;
+
+    public static Range RangeUnion(Range x, Range y) => (Math.Min(x.a, y.a), Math.Max(x.b, y.b));
+
+    public static ulong RangeLength(Range r) => r.b - r.a + 1;
+}
