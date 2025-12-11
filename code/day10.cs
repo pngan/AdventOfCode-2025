b27dedfca0e7ed68db0ba0cb13158ccd4fbf696f
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-12-11 22:06:48 +1300

    [2025 Day 10]

---
+++ code/day10.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Immutable;
+using MoreLinq;
+
+public class Day10
+{
+    public const string Day = "10";
+    public static object Input()
+    {
+        var input = File.ReadLines($"input/2025_{Day}_input.txt");
+
+        return input;
+    }
+
+    public static object Solve1()
+    {
+        var input = Input();
+
+        return input;
+    }
+
+    public static object Solve2()
+    {
+        var input = Input();
+
+        return input;
+    }
+}
